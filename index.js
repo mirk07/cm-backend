95c5032be888c9c0001641c5d294ec9866574a36
mirk07 <93538327+mirk07@users.noreply.github.com>
2023-03-19 17:10:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const multer = require("multer");
 const cors = require("cors");
+const fs = require("fs");
 
 const {
   registerValidation,
